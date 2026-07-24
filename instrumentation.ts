df1845b6e5898f06359c696703104d7183f81747
clebermarcolino <clebermarcolinogba@gmail.com>
2026-07-23 23:01:29 -0300

    feat(observability): adicionando instrumentação automática OpenTelemetry Grafana LGTM

---
+++ instrumentation.ts
@@ -0,0 +1,21 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const sdk = new NodeSDK({
+      serviceName: process.env.OTEL_SERVICE_NAME ?? 'eq04-alelib',
+      traceExporter: new OTLPTraceExporter({
+        url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`,
+      }),
+      instrumentations: [
+        getNodeAutoInstrumentations({
+          '@opentelemetry/instrumentation-fs': { enabled: false },
+        }),
+      ],
+    });
+
+    sdk.start();
+  }
+}
